af1eebc31c1ba12202f5da67cfd912b0bf6de2fe
Darlley <37590954+Darlley@users.noreply.github.com>
2018-07-04 20:49:51 -0400

    Update challenge-06.js

--- challenge-06/challenge-06.js
+++ challenge-06/challenge-06.js
@@ -4,7 +4,8 @@ para começar o desafio.
 Declare uma variável chamada `championship` que receberá o nome do campeonato,
 e imprima o nome desse campeonato no console.
 */
-// ?
+var championship = "campeonato brasileiro";
+console.log(championship);
 
 /*
 Declare uma variável chamada `teams`, que receberá um array com 5 elementos.
@@ -12,7 +13,7 @@ Os elementos serão nomes de times do campeonato escolhido, e os nomes devem
 estar na ordem em que eles aparecem na tabela no momento da solução desse
 desafio.
 */
-// ?
+var teams = ['São Paulo', 'Santos', 'Palmeiras','Corinthians','Gremio'];
 
 console.log( 'Times que estão participando do campeonato:', teams );
 
@@ -32,20 +33,32 @@ Crie uma função chamada `showTeamPosition` com as seguintes características:
     - Se não houver time para a posição passada, deve retornar a mensagem:
     "Não temos a informação do time que está nessa posição."
 */
-// ?
-
+function showTeamPosition(Position){
+  if (Position <= 5){
+    var mensagem = "O time que está em " + Position + "º lugar é o " + teams[x] ".";
+  }else{
+    var mensagem = "Não temos a informação do time que está nessa posição."
+  }
+  
+  console.log(mensagem);
+};
 /*
 Escolha 4 times do campeonato selecionado e mostre a posição dele, usando a
 função acima. Entre esses 4, adicione 1 que não esteja entre os 5 primeiros.
 */
-// ?
+showTeamPosition(1); //"O time que está em 1º lugar é o São Paulo."
+showTeamPosition(2); //"O time que está em 2º lugar é o Santos."
+showTeamPosition(3); //"O time que está em 3º lugar é o Palmeiras."
+showTeamPosition(6); //"Não temos a informação do time que está nessa posição."
 
 /*
 Mostre os números de 20 a 30 no console (inclusive o 30), usando a estrutura de
 repetição "while".
 */
-// ?
-
+var iniciadora = 20;
+while(iniciadora <= 30){
+  console.log(iniciadora++)
+};
 /*
 Crie uma função chamada `convertToHex`, com as seguintes características:
     - A função recebe uma cor por parâmetro, do tipo string. Exemplo: "red";
